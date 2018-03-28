3509a3d9600185ae5b916c9afdac0f9062bc59b1
Yulya <xenerizes@users.noreply.github.com>
2018-03-28 17:03:56 +0300

    Extended read_and_plot to work with DcStats too

--- scripts/read_and_plot.py
+++ scripts/read_and_plot.py
@@ -6,7 +6,7 @@ import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from forecast.cstats import ControlStats
+from forecast.cstats import ControlStats, DcStats
 
 
 def plot(frame):
@@ -17,11 +17,18 @@ def plot(frame):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'file', type=str
+        'file', type=str, help='CSV input'
+    )
+    parser.add_argument(
+        '--dc', action='store_true', help='Specify if input is not Runos'
     )
     opts = parser.parse_args(sys.argv[1:])
 
     frame = pd.read_csv(opts.file, index_col=[0])
-    stats = ControlStats(frame)
-    aggregated = stats.aggregate()
-    plot(aggregated.diff())
+    if opts.dc:
+        stats = DcStats(frame)
+        plot(stats.summary())
+    else:
+        stats = ControlStats(frame)
+        aggregated = stats.aggregate()
+        plot(aggregated.diff())
