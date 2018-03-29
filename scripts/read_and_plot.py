490b051963cf3a9df1971cfd0575f5914cdca205
Yulya <xenerizes@users.noreply.github.com>
2018-03-29 16:52:20 +0300

    Better plots

--- scripts/read_and_plot.py
+++ scripts/read_and_plot.py
@@ -14,6 +14,8 @@ from forecast.cstats import ControlStats, DcStats
 
 def shape(x):
     ncols = math.ceil(pow(x, 0.5))
+    if ncols > 4:
+        ncols = 4
     nrows = math.ceil(x / ncols)
     return int(ncols), int(nrows)
 
@@ -21,16 +23,16 @@ def shape(x):
 def add_subplots(datasets, time_visibility=False):
     ncols, nrows = shape(len(datasets))
     for idx, dataset in enumerate(datasets):
+        title = '{}: {} - {}'.format(idx, dataset.devname[0], dataset.infname[0])
         sp = plt.subplot(nrows, ncols, idx + 1)
-        dataset.plot(ax=sp, grid=True)
+        dataset.plot(ax=sp, grid=True, title=title)
         sp.get_xaxis().set_visible(time_visibility)
 
 
-def plot_dc(frame, time_visibility=False):
-    stats = DcStats(frame).summary()
+def plot_dc(frame, time_visibility=False, type=0):
+    stats = DcStats(frame).summary(type)
     plt.subplots_adjust(left=0.05, bottom=0.1,
-                        right=0.98, top=0.95,
-                        hspace=0.5, wspace=0.5)
+                        right=0.98, top=0.95)
     add_subplots(stats, time_visibility)
     plt.show(block=True)
 
