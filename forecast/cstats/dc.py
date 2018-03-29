65ab16e7671afe90ebd1eaa6331c9e91034dbb15
Yulya <xenerizes@users.noreply.github.com>
2018-03-29 16:52:50 +0300

    DcStats handles devices too

--- forecast/cstats/dc.py
+++ forecast/cstats/dc.py
@@ -18,9 +18,22 @@ class DcStats(object):
         filtered = self.frame.devname.drop_duplicates()
         return filtered.reset_index().drop('time', axis=1).devname
 
-    def summary(self):
+    def inf_list(self):
+        filtered = self.frame.infname.drop_duplicates()
+        return filtered.reset_index().drop('time', axis=1).infname
+
+    def summary(self, type=0):
+        if type is 1:
+            return self.inf_summary()
         data = list()
         for dev in self.dev_list():
             dev_records = self.frame.loc[self.frame['devname'] == dev]
+            data.append(dev_records)
+        return data
+
+    def inf_summary(self):
+        data = list()
+        for inf in self.inf_list():
+            dev_records = self.frame.loc[self.frame['infname'] == inf]
             data.append(dev_records)
         return data
