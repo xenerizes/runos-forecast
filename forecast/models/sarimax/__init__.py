3512792bd069cdc10d22b81da4d1e786c9d33919
xenerizes <xenerizes@users.noreply.github.com>
2018-03-31 14:01:48 +0300

    Added SARIMAX model

---
+++ forecast/models/sarimax/__init__.py
@@ -0,0 +1,1 @@
+from .sarimax import SARIMAXModel
---
+++ forecast/models/sarimax/sarimax.py
@@ -0,0 +1,17 @@
+from pandas.tseries.offsets import Second
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+
+from ..arima import ARIMAModel
+
+
+class SARIMAXModel(ARIMAModel):
+    def get_fitted_values(self):
+        return self._model.fittedvalues()
+
+    def auto(self, order=None):
+        self._period = self._ts.index[1] - self._ts.index[0]
+        freq = Second(self._period.total_seconds())
+        self._order = order if order is not None else self.select_order()
+        self._model = SARIMAX(self._ts, order=self._order, freq=freq).fit()
+
+
