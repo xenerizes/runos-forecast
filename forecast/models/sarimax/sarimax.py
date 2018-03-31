fbb63164c219d0eb1b68356b2c9c47a0597acaf0
xenerizes <xenerizes@users.noreply.github.com>
2018-03-31 21:36:46 +0300

    Disabled stationarity and invertability enforcement

--- forecast/models/sarimax/sarimax.py
+++ forecast/models/sarimax/sarimax.py
@@ -4,9 +4,13 @@ from ..arima import ARIMAModel
 
 
 class SARIMAXModel(ARIMAModel):
-    def auto(self, order=None, seasonal_order=None):
+    def auto(self, order=None, seasonal_order=None, start_params=None):
         self._period = self._ts.index[1] - self._ts.index[0]
         self._order = order if order is not None else self.select_order()
-        self._model = SARIMAX(self._ts, order=self._order, seasonal_order=seasonal_order).fit()
+        self._model = SARIMAX(self._ts, order=self._order,
+                              seasonal_order=seasonal_order,
+                              enforce_stationarity=False,
+                              enforce_invertibility=False).fit(start_params)
 
-
+    def simulate(self, n):
+        return self._model.simulate(n)
