e828973f9ae3692659528b8a7ab692c8ee04c626
xenerizes <xenerizes@users.noreply.github.com>
2018-03-31 22:40:41 +0300

    Removed absolute errors

--- forecast/models/base/quality.py
+++ forecast/models/base/quality.py
@@ -5,31 +5,13 @@ class ForecastQuality(object):
     def __init__(self, actual, fit):
         self._actual = actual
         self._fit = fit
-
-    def mae(self):
-        return ml_metrics.mae(self._actual, self._fit)
-
-    def rmse(self):
-        return ml_metrics.rmse(self._actual, self._fit)
-
-    def ae(self):
-        return ml_metrics.ae(self._actual, self._fit)
-
-    def mse(self):
-        return ml_metrics.mse(self._actual, self._fit)
-
-    def se(self):
-        return ml_metrics.se(self._actual, self._fit)
+        self.mae = ml_metrics.mae(self._actual, self._fit)
+        self.rmse = ml_metrics.rmse(self._actual, self._fit)
+        self.mse = ml_metrics.mse(self._actual, self._fit)
 
     def summary(self):
-        res = {
-            'Absolute Error': self.ae(),
-            'Squared Error': self.se(),
-            'Mean Absolute Error': self.ae(),
-            'Mean Squared Error': self.mse(),
-            'Root Mean Squared Error': self.rmse()
+        return {
+            'Mean Absolute Error': self.mae,
+            'Mean Squared Error': self.mse,
+            'Root Mean Squared Error': self.rmse
         }
-
-        print("{:<15} {:<10}".format('Error', 'Score'))
-        for k, v in res.items():
-            print("{:<15} {:<10}".format(k, v))
