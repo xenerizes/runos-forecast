dbbd02bd6ed87d3a552a3cb13f87c0a4f526cf5f
xenerizes <xenerizes@users.noreply.github.com>
2018-03-31 16:22:58 +0300

    Added seasonal order

--- forecast/models/sarimax/sarimax.py
+++ forecast/models/sarimax/sarimax.py
@@ -7,9 +7,9 @@ class SARIMAXModel(ARIMAModel):
     def get_fitted_values(self):
         return self._model.fittedvalues()
 
-    def auto(self, order=None):
+    def auto(self, order=None, seasonal_order=None):
         self._period = self._ts.index[1] - self._ts.index[0]
         self._order = order if order is not None else self.select_order()
-        self._model = SARIMAX(self._ts, order=self._order).fit()
+        self._model = SARIMAX(self._ts, order=self._order, seasonal_order=seasonal_order).fit()
 
 
