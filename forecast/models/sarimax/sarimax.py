af099dd37329ab0932e3b34582be5a26c410459d
Yuliya <xenerizes@users.noreply.github.com>
2018-05-03 17:48:08 +0300

    Change base class of SARIMAXModel from ARIMAModel to Model

--- forecast/models/sarimax/sarimax.py
+++ forecast/models/sarimax/sarimax.py
@@ -1,14 +1,32 @@
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
+from ..base import Model
 from ..arima import ARIMAModel
 
 
-class SARIMAXModel(ARIMAModel):
+class SARIMAXModel(Model):
+    def __init__(self, ts):
+        Model.__init__(self, ts)
+        self._order = None
+        self._resid = None
+        self._seasonal_order = None
+
+    def select_order(self):
+        arima = ARIMAModel(self._ts)
+        arima.auto()
+        self._resid = arima._model.resid
+        return arima.select_order()
+
+    def select_seasonal_order(self):
+        arima_resid = ARIMAModel(self._resid)
+        return arima_resid.select_order()
+
     def auto(self, order=None, seasonal_order=None, start_params=None):
         self._period = self._ts.index[1] - self._ts.index[0]
         self._order = order if order is not None else self.select_order()
+        self._seasonal_order = seasonal_order if seasonal_order is not None else self.select_seasonal_order()
         self._model = SARIMAX(self._ts, order=self._order,
-                              seasonal_order=seasonal_order,
+                              seasonal_order=self._seasonal_order,
                               enforce_stationarity=False,
                               enforce_invertibility=False).fit(start_params)
 
