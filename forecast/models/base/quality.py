4a77f019a9e099c098b799e139d26ab19cfcc3d2
Yuliya <xenerizes@users.noreply.github.com>
2018-05-15 15:14:03 +0300

    Codestyle

--- forecast/models/base/quality.py
+++ forecast/models/base/quality.py
@@ -3,11 +3,11 @@ import ml_metrics
 
 class ForecastQuality(object):
     def __init__(self, actual, fit):
-        self._actual = actual
-        self._fit = fit
-        self.mae = ml_metrics.mae(self._actual, self._fit)
-        self.rmse = ml_metrics.rmse(self._actual, self._fit)
-        self.mse = ml_metrics.mse(self._actual, self._fit)
+        self.actual = actual
+        self.fit = fit
+        self.mae = ml_metrics.mae(self.actual, self.fit)
+        self.rmse = ml_metrics.rmse(self.actual, self.fit)
+        self.mse = ml_metrics.mse(self.actual, self.fit)
 
     def summary(self):
         return {
