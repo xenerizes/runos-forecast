404cac578b1530cb4060d6ccb17cf5c9962f3bff
Yuliya <xenerizes@users.noreply.github.com>
2019-04-13 22:26:07 +0300

    changed quality metrics

--- forecast/models/base/quality.py
+++ forecast/models/base/quality.py
@@ -1,16 +1,20 @@
-from ml_metrics import mae, rmse, mse
+from ml_metrics import mse, ae
 from pandas import Series
+import numpy as np
+
+
+def mape(actual, predicted):
+    return np.mean(ae(actual, predicted)/actual)
 
 
 class ForecastQuality(object):
     def __init__(self, actual, fit):
         self.actual = actual
         self.fit = fit
-        self.mae = mae(self.actual, self.fit)
-        self.rmse = rmse(self.actual, self.fit)
         self.mse = mse(self.actual, self.fit)
+        self.mape = mape(self.actual, self.fit)
 
     def summary(self):
-        data = [self.mae, self.mse, self.rmse]
-        index = ['Mean Absolute Error', 'Mean Squared Error', 'Root Mean Squared Error']
+        data = [self.mse, self.mape]
+        index = ['Mean Squared Error', 'Mean Absolute Percentage Error']
         return Series(data, index=index)
