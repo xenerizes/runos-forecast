fixed load calculation

forecast/models/load/load.py
@@ -1,12 +1,11 @@
 from ..base import Model
 from functools import reduce
-import numpy as np
 
 
 def _compute_ts(storage):
     ts = reduce(lambda x, y: x + y, storage.switch_load().values())
     const_load = map(lambda x: x.min(), storage.const_load().values())
-    return ts + np.min(const_load)
+    return ts + reduce(lambda x, y: x + y, const_load)
 
 
 class LoadModel(Model):
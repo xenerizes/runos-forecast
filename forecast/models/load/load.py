5b43242d3861f1328935c49eceeb503d0535726e
xenerizes <xenerizes@users.noreply.github.com>
2019-04-16 12:56:13 +0300

    fixed load calculation

--- forecast/models/load/load.py
+++ forecast/models/load/load.py
@@ -1,12 +1,11 @@
 from ..base import Model
 from functools import reduce
-import numpy as np
 
 
 def _compute_ts(storage):
     ts = reduce(lambda x, y: x + y, storage.switch_load().values())
     const_load = map(lambda x: x.min(), storage.const_load().values())
-    return ts + np.min(const_load)
+    return ts + reduce(lambda x, y: x + y, const_load)
 
 
 class LoadModel(Model):
