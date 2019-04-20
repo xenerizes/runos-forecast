7786813edde8f82e8dd8a1f56e6d42d42c889afa
Yuliya <xenerizes@users.noreply.github.com>
2019-04-21 01:45:33 +0300

    compute ts as method

--- forecast/models/load/load.py
+++ forecast/models/load/load.py
@@ -2,12 +2,6 @@ from ..base import Model
 from functools import reduce
 
 
-def _compute_ts(storage):
-    ts = reduce(lambda x, y: x + y, storage.switch_load().values())
-    const_load = map(lambda x: x.min(), storage.const_load().values())
-    return ts + reduce(lambda x, y: x + y, const_load)
-
-
 class LoadModel(Model):
     def __init__(self, storage, components):
         Model.__init__(self, storage.load())
@@ -15,4 +9,9 @@ class LoadModel(Model):
         self.storage = storage
 
     def predict(self, length):
-        return _compute_ts(self.storage)
+        return self._compute_ts()
+
+    def _compute_ts(self):
+        ts = reduce(lambda x, y: x.add(y, fill_value=0), self.components)
+        const_load = list(map(lambda x: x.min(), self.storage.const_load().values()))
+        return ts + sum(const_load)
