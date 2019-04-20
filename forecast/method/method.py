fb87ab127a4ce26f27b9a4cf8a97580a56127ac7
Yuliya <xenerizes@users.noreply.github.com>
2019-04-21 00:26:01 +0300

    log stats and save histograms about time

--- forecast/method/method.py
+++ forecast/method/method.py
@@ -1,11 +1,17 @@
 import logging
 from time import strftime, localtime
-from pandas import DataFrame
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from forecast.algorithm import AggregationAlgorithm
 
 TIME_FORMAT = "%Y-%m-%d_%H-%M"
+
+
+def print_time_summary(ts):
+    res = pd.Series([ts.min(), ts.max(), ts.mean(), ts.median()],
+                    index=['Min', 'Max', 'Mean', 'Median'])
+    logging.info(res.__str__())
 
 
 class LoadForecastMethod(object):
@@ -21,11 +27,13 @@ class LoadForecastMethod(object):
             try:
                 sw_algos = {sw: self.algo_class(self.model_class, data, self.opts)
                             for sw, data in ts.switch_load().items()}
+                full_time_data = []
                 for sw, algo in sw_algos.items():
                     logging.info('Forecasting load from switch {}...'.format(sw))
                     algo.run()
                     logging.info('Forecast quality for series {}, switch {}:'.format(idx, sw))
                     algo.print_quality()
+                    full_time_data += algo.time_stats()
                 lm = AggregationAlgorithm(ts, [a.forecast for a in sw_algos.values()], self.opts)
                 logging.info('Forecasting summary load...')
                 lm.run()
@@ -35,8 +43,8 @@ class LoadForecastMethod(object):
 
                 algo_quality = {'{}'.format(sw): sw_algos[sw].quality_stats() for sw in sw_algos.keys()}
                 algo_quality['full'] = lm.quality_stats()
-                quality_frame = DataFrame(algo_quality)
-                quality_frame.hist(figsize=(20, 15), grid=True)
+                quality_frame = pd.DataFrame(algo_quality)
+                quality_frame.hist(figsize=(20, 15), grid=True, title=str(idx))
                 plt.savefig('hist-{}-{}.png'.format(idx, strftime(TIME_FORMAT, localtime())),
                             bbox_inches='tight')
                 plt.close()
@@ -44,6 +52,14 @@ class LoadForecastMethod(object):
                 lm.data.plot(figsize=(20, 15), grid=True, title=str(idx))
                 lm.forecast.plot(figsize=(20, 15), grid=True, title=str(idx))
                 plt.savefig('figure-{}-{}.png'.format(idx, strftime(TIME_FORMAT, localtime())),
+                            bbox_inches='tight')
+                plt.close()
+
+                time_ts = pd.Series(full_time_data)
+                logging.info('Fitting time information for series {}'.format(idx))
+                print_time_summary(time_ts)
+                time_ts.hist(figsize=(20, 15), grid=True, title=str(idx))
+                plt.savefig('time-{}-{}.png'.format(idx, strftime(TIME_FORMAT, localtime())),
                             bbox_inches='tight')
                 plt.close()
 
